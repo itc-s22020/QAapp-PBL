95a7d315ec9eb8ef7edb1b04c2b15c9c3a845f0a
itc-s22020 <s22020@std.it-college.ac.jp>
2023-08-10 01:52:56 +0000

    [add]radio button.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
+import styles from "@/styles/Home.module.css"
+import { useState } from "react";
+
 const Home = () => {
+  const RADIO_VALUES = ["新着", "人気"];
+  const [selectedRadioBtnValue, setSelectedRadioBtnValue] = useState("");
+  const onRadioBtnChanged = (e) => setSelectedRadioBtnValue(e.target.value);
+
   return (
     <>
-      <main>topPage</main>
+      {RADIO_VALUES.map((radioValue) => (
+        <label key={radioValue}>
+          <input
+            type="radio"
+            value={radioValue}
+            name="sample"
+            onChange={onRadioBtnChanged}
+          />
+          {radioValue}
+        </label>
+      ))}
+      <div>check : {selectedRadioBtnValue}</div>
     </>
-
   )
 }
+
 export default Home
