bacdc31ffce98796a2ea59a3c508aff5b39e036e
itc-s22012 <s22012@std.it-college.ac.jp>
2023-08-10 01:20:32 +0000

    回答完了ページ

---
+++ src/pages/Answer_completion.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import styles from '../styles/Answer_completion.module.css'; 
+
+const Answer = () => {
+    return (
+	    
+        <div className={styles.center_content}>
+            <h1 className={styles.centerText}>回答を投稿しました</h1>
+            <button className={styles.center_button}>質問ページに戻る</button>
+        </div>
+    );
+}
+export default Answer;
