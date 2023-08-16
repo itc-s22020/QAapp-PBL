be177354ecbe7406b220982bb12355ff36615db9
s22030 <s22030@std.it-college.ac.jp>
2023-08-16 00:38:09 +0000

    Header画像貼って間隔まであけた

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,42 @@
 import Image from 'next/image'
 import logo from '../images/homelogo.png'
+import rank from '../images/ranklogo.png'
+import profile from '../images/prologo.png'
+import logout from '../images/lgout.png'
 
 export default function Header(){
 	return (
+		<>
 		<Image
 		src={logo}
 		width={300}
 		height={100}
+		style={{marginRight: "40px"}}
 		alt=''
 		/>
+		<Image
+		src={rank}
+		width={150}
+		height={100}
+		style={{marginRight: "40px"}}
+		alt=''
+		/>
+		<Image
+		src={profile}
+		width={150}
+		height={100}
+		style={{marginRight: "40px"}}
+		alt=''
+		/>
+		<Image
+		src={logout}
+		width={150}
+		height={100}
+		style={{marginRight: "40px"}}
+		alt=''
+		/>
+		</>
+
 	)
 }
+
