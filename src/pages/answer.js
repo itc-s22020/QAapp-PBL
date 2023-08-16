6296933c8d58db5400ed2041204eefb151adaad8
itc-s22012 <s22012@std.it-college.ac.jp>
2023-08-16 03:03:05 +0000

    ページの名前変更

--- src/pages/answer.js
+++ src/pages/answer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/Answer_completion.module.css'; 
+import styles from '../styles/answer.module.css'; 
 import Link from 'next/link'
 
 const Answer = () => {
