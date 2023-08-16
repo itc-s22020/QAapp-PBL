88f488353e295da9e1d51df4aff14ed6e1192693
itc-s22012 <s22012@std.it-college.ac.jp>
2023-08-16 01:58:15 +0000

    ボタンの機能を実装

--- src/pages/Answer_completion.js
+++ src/pages/Answer_completion.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from '../styles/Answer_completion.module.css'; 
-import { Link } from './index'
+import Link from 'next/link'
 
 const Answer = () => {
     return (
             
         <div className={styles.center_content}>
             <h1 className={styles.centerText}>回答を投稿しました</h1>
+	    <Link href="/index">
             <button className={styles.center_button}>質問ページに戻る</button> 
+	    </Link>
         </div>
     );
 }
