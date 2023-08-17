8341d95e7aa4a975c9a41d0b815cde47386658fa
itc-s22020 <s22020@std.it-college.ac.jp>
2023-08-17 02:10:34 +0000

    [mkdir]lib

---
+++ src/lib/formatDate.js
@@ -0,0 +1,11 @@
+const formatDate = (s) => {
+    const d = new Date(s)
+    const [year, month, day, hour, minute] = [
+        d.getFullYear(),
+        d.getMonth(),
+        d.getDate(),
+        d.getHours(),
+        d.getMinutes()
+    ]
+    return `${year}/${month}/${day} ${hour}:${minute}`
+}
