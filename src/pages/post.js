b7a32eb47aaef44d147b4352a2c6f48cc5223e95
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-08-19 11:45:28 +0900

    カテゴリを取得して表示した

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,10 +1,24 @@
 import {Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
 const PostPage = () => {
     const [title, setTitle] = useState('')
     const [categoryId, setCategoryId] = useState('')
     const [text, setText] = useState('')
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/category`)
+            .then((r) => r.json())
+            .then((d) => setData(d))
+    }, [setData])
+
+    const CategorySelect = () =>
+        <Select label={"カテゴリ"} defaultValue={categoryId} onChange={(e) => setCategoryId(e.target.value)}>
+            {data.map((c) => <MenuItem key={c.id} value={c.id}>{c.name}</MenuItem>)}
+        </Select>
+
     return (
         <>
             <Box sx={{backgroundColor: "#F6EEEE", color: "#2B2C34", p: 1}}>
@@ -15,10 +29,7 @@ const PostPage = () => {
                     </FormControl>
                     <FormControl sx={{my: 1}} fullWidth>
                         <InputLabel>カテゴリ</InputLabel>
-                        <Select label={"カテゴリ"} value={categoryId} onChange={(e) => setCategoryId(e.target.value)}>
-                            <MenuItem value={1}>勉強</MenuItem>
-                            <MenuItem value={2}>趣味</MenuItem>
-                        </Select>
+                        <CategorySelect/>
                     </FormControl>
                     <FormControl sx={{my: 1}} fullWidth>
                         <TextField variant={"outlined"} label={"内容"} multiline minRows={5} onChange={(e) => setText(e.target.value)}/>
