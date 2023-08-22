924926a307b7db140ec61a3abefe322cc36cc606
itc-s22020 <s22020@std.it-college.ac.jp>
2023-08-22 02:22:16 +0000

    使ってないやつ消した

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,6 @@ import IconLiked from '@mui/icons-material/Favorite';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
 
 const Home = () => {
   const [alignment, setAlignment] = React.useState('new');
@@ -60,31 +55,6 @@ const Home = () => {
       <div>人気</div>
     </ToggleButton>,
   ];
-
-
-  const Cards = ({ q_id, title, c_name, date, like }) => (
-    <div>
-      <React.Fragment>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="div">
-            <div>test</div>
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-      </React.Fragment>
-    </div>
-  );
-
 
   //質問単体の表示
   const Prop = ({ q_id, title, c_name, date, like }) => (
