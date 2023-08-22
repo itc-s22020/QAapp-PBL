cdb0e0f1be975309254483b05452e16390f27bc7
s22013 <s22013@std.it-college.ac.jp>
2023-08-22 12:14:25 +0900

    プロフィールページ作成

--- src/components/answer_profile.js
+++ src/components/answer_profile.js
@@ -3,15 +3,15 @@ import styles from '../styles/profileanswer.module.css';
 
 const AnswerProfile = () => {
   return (
+    <div>	  
     <div className={styles.box1}>
       <h2 className={styles.h2}>最近の回答</h2>
-      <hr className={styles.horizontalLine} />	  
     <div className={styles.botam}>	  
       <button className={`${styles.button} ${styles.button01}`}>全ての投稿を見る</button>
     </div>
     </div>
+    </div>	  
   );
 };
 
 export default AnswerProfile;
-
