8cd1eefc43cd5f1c8c97ab761bca765869e6caa2
s22030 <s22030@std.it-college.ac.jp>
2023-08-22 01:36:07 +0000

    icon貼った、header.cssを作った

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,34 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import logo from '../images/homelogo.png'
-import rank from '../images/ranklogo.png'
-import profile from '../images/prologo.png'
-import logout from '../images/lgout.png'
+// import rank from '../images/ranklogo.png'
+// import profile from '../images/prologo.png'
+// import logout from '../images/lgout.png'
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonIcon from '@mui/icons-material/Person';
+import axios from "axios"
+import "../styles/header.css";
 
 export default function Header(){
+	const handleLogout = async(e) => {
+     		e.preventDefault()
+		try {
+			const loginCheck = await axios.get(`${API_HOST}/api/user/login`,{withCredentials:true})
+			if (loginCheck.status !== 200){
+				return
+			}
+			await axios.get(`{API_HOST}/api/user/logout`,{
+				withCredentials:true
+			})
+		}catch(e){
+			console.log(e)
+		}
+	}
 	return (
-		<>
-		<Image
-		src={logo}
-		width={300}
-		height={100}
-		style={{marginRight: "40px"}}
-		alt=''
-		/>
-		<Image
-		src={rank}
-		width={150}
-		height={100}
-		style={{marginRight: "40px"}}
-		alt=''
-		/>
-		<Image
-		src={profile}
-		width={150}
-		height={100}
-		style={{marginRight: "40px"}}
-		alt=''
-		/>
-		<Image
-		src={logout}
-		width={150}
-		height={100}
-		style={{marginRight: "40px"}}
-		alt=''
-		/>
-		</>
-
-	)
+    	<div className='wrap'>
+		<LeaderboardIcon className='icon' />
+        	<PersonIcon className='icon' />
+        	<LogoutIcon className='icon' />
+    	</div>
+  	)
 }
-
